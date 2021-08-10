a0e1e3a739e4399aababd79ec744ec8200dffb22
Wilson Santos <88521540+wilsonsantosact@users.noreply.github.com>
2021-08-10 16:31:48 -0300

    criação de blog
    criação do esqueleto aplicativo blog com nodejs

---
+++ blogapp/app.js
@@ -0,0 +1,38 @@
+//Carregando módulos
+
+const express = require('express');
+const handlebars = require('express-handlebars');
+const bodyparser = require('body-parser');
+const app = express();
+const admin = require('./routes/admin');
+
+//const mongoose = require('mongoose');
+
+//Configurações
+//body parser
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+//Handlebars
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+//mongoose
+//em breve
+
+//Rotas
+app.get('/', (req, res) => {
+    res.send('Rota Principal!')
+});
+
+app.get('/posts', (req, res) => {
+    res.send('Lista Posts')
+})
+
+app.use('/admin', admin);
+
+//Outros
+const port = 8081
+app.listen(port, () => {
+    console.log("Servidor rodando!");
+})
